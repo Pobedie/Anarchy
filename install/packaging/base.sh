df855d0c860730756731f63cbf93c42caecb6fba
Pobedie <nope@nope>
2026-07-11 16:57:39 +0200

    Fallback from pacman to yay in packaging stage

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,5 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
-omarchy-pkg-add "${packages[@]}"
+if !omarchy-pkg-add "${packages[@]}"; then
+  omarchy-pkg-aur-add "${packages[@]}"
+fi
